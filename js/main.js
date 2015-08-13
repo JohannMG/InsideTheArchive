10c56e84e35e71c2a9f2f9f7b96848b0079828ce
JohannMG <hello@johannmg.com>
2015-08-13 15:25:46 -0400

    first

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var itemList;
+var tile  = $("#tile-item").html();
+var tileItemTemplate = Handlebars.compile(tile);
+var renderedTiles; 
+
+
+$.ajax({
+  url: "/items-data.json",
+  data: {},
+  dataType: "json",
+  success: function( data ) {
+    itemList = data;
+    renderedTiles = tileItemTemplate(itemList);
+    var tileContainer = document.getElementById('tiles-container'); 
+    tileContainer.innerHTML = renderedTiles;
+  },
+  error: function(jqXHR, textStatus, errorThrown){
+  	console.log(errorThrown);
+  }
+});
+
