6edbf4d0bcd31667033870520a3adb723a56c11c
JohannMG <hello@johannmg.com>
2015-08-17 14:53:29 -0400

    photo LB binding

--- js/add-light-box.js
+++ js/add-light-box.js
@@ -12,12 +12,38 @@
 ----------------*/
 
 
+///colorbox STANDARDS
+var LB_SETTINGS = {
+	FADEIN_SPEED: function () { return 150; }, 
+	SCROLLING: function () { return true; }, 
+	OPATICY: function () { return 0.75; }, 
+	FADEOUT_SPEED: function () { return 150; }, 
+	
+};
+///end colorbox settings
+
+
 //PHOTO media
 function attachPhotoLightboxes () {
-	
+	var photoItemsArray = $('.media-image'); 
+
+	_.forEach(photoItemsArray, function(element, indes, list){
+
+		var media = element.getAttribute('media'); 
+		var imgHTML =  '<div class="lightboxHTML"><img class="lb" src="' + media + '"/></div>'; 
+
+		$(element).colorbox({
+			html: imgHTML, 
+			speed: LB_SETTINGS.FADEIN_SPEED(), 
+			scrolling: LB_SETTINGS.SCROLLING(), 
+			opacity: LB_SETTINGS.OPATICY(),
+			fadeOut: LB_SETTINGS.FADEOUT_SPEED()
+		}); 
+	});
 }
 
 
 
 //method calls async
 attachPhotoLightboxes(); 
+
