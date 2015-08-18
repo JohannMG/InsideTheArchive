1cdd102682ec791f5e1085cb325e22bba6d1338d
JohannMG <hello@johannmg.com>
2015-08-18 19:22:14 -0400

    html tuneups, more artifacts

--- js/items-list.js
+++ js/items-list.js
@@ -15,34 +15,27 @@ var tileDataJSON = {
 function itemsJSON () {
 return[
 
-	{	"id": "tileitem-01",
+	{	"id": "weirdtaxidermy",
 		"title": "Weird Taxidermy",
 		"description": "Artist Inflated Animals into Colorful Balloons", 
 		"type": "image",
 		"mediaURL": "http://www.ripleys.com/wp-content/uploads/2015/07/This-Horse-is-said-to-represent-harmony-harmoneigh.png"
 	},
 	{
-		"id": "tileitem-02",
-		"title": "Weird Taxidermy",
-		"description": "Weird Taxidermy: Artist Inflated Animals into Colorful Balloons", 
-		"type": "image",
-		"mediaURL": "http://www.ripleys.com/wp-content/uploads/2015/08/Waffle-House-Index-Ripleys-Header.jpg" 
+		"id": "longskull",
+		"title": "Elongated Skulls", 
+		"description": "Believe It or Not, the Ancient Moche of Peru performed difficult brain operations over 1400 years ago, and typically bound the skulls of babies.",
+		"type": "spin",
+		"spinEmbed": "<iframe src='//spins0.arqspin.com/iframe.html?spin=jd1qjxd21onb&is=-0.16' width=950 height=950 scrolling='no' frameborder='0'></iframe>",
+		"mediaURL":"http://www.ripleys.com/wp-content/uploads/2015/08/Elongated-Peruviant-Skull.jpg" 
 	},
 	{
-		"id": "tileitem-03",
-		"title": "Tibetan Ram Skull", 
+		"id": "vampirekiller",
+		"title": "Vampire Killing Kit", 
 		"description": "Used by Lamaist monks during ritual ceremonies, animal votive skulls are cherished as religious relics in Tibet in the belief that the soul of the animal resides ",
 		"type": "spin",
-		"spinEmbed": "<iframe src='//spins0.arqspin.com/iframe.html?spin=esmxi9bojr6m&is=-0.16' width=950 height=950  frameborder='0'></iframe>",
-		"mediaURL":"http://www.ripleys.com/wp-content/uploads/2015/06/Ripcycle-Entires-Week-3-Unbelievable-Art-Contest.jpg" 
-	},
-	{
-		"id": "tileitem-04",
-		"title": "Tibetan Ram Skull", 
-		"description": "Used by Lamaist monks during ritual ceremonies, animal votive skulls are cherished as religious relics in Tibet in the belief that the soul of the animal resides ",
-		"type": "spin",
-		"spinEmbed": "<iframe src='//spins0.arqspin.com/iframe.html?spin=esmxi9bojr6m&is=-0.16' width=950 height=950 scrolling='no' frameborder='0'></iframe>",
-		"mediaURL": "http://www.ripleys.com/wp-content/uploads/2015/06/Ripcycle-Entires-Week-3-Unbelievable-Art-Contest.jpg"
+		"spinEmbed": "<iframe src='//spins0.arqspin.com/iframe.html?spin=8do2purcmqqs&is=-0.16' width=950 height=950 scrolling='no' frameborder='0'></iframe>",
+		"mediaURL": "http://www.ripleys.com/wp-content/uploads/2011/10/Vampire-Killing-Kit-More-HOly-Water.jpg"
 	},
 	{
 		"id": "tileitem-05",
@@ -51,6 +44,22 @@ return[
 		"type": "videoEmbed",
 		"videoEmbed": "<iframe width='853' height='480' src='https://www.youtube-nocookie.com/embed/kqCPIDrPic8?autoplay=1&list=PL3fZl9yFVeRLJSAVEFPhrBe0d5hMRWtwu&amp;controls=0&amp;showinfo=0' frameborder='0' allowfullscreen></iframe>",
 		"mediaURL": "http://www.ripleys.com/wp-content/uploads/2014/10/Katzen-With-Jeep-Ripleys-BION.jpg" 
+	},
+	{
+		"id": "shrunkenhead360",
+		"title": "Genuine Shrunken Head", 
+		"description": "Believe It or Not!, this head came from a normal-sized person! The Shuar (Jivaro in Spanish) Indians of Ecuador claimed their enemies heads as symbols of bravery.",
+		"type": "spin",
+		"spinEmbed": "<iframe src='//spins0.arqspin.com/iframe.html?spin=xzgnkxiqy5i7&is=-0.16' width=950 height=950 scrolling='no' frameborder='0'></iframe>",
+		"mediaURL": "http://www.ripleys.com/wp-content/uploads/2015/08/Shrunken-Head-360-Spin.jpg" 
+	},
+	{
+		"id": "twoheadcalf",
+		"title": "Two Headed Calf", 
+		"description": "Robert Ripley affectionately called these genetic mutations \"pranks of nature\"",
+		"type": "spin",
+		"spinEmbed": "<iframe src='//spins0.arqspin.com/iframe.html?spin=18j79clvm3xfz&is=-0.16' width=950 height=950 scrolling='no' frameborder='0'></iframe>",
+		"mediaURL": "http://www.ripleys.com/wp-content/uploads/2015/08/Shrunken-Head-360-Spin.jpg" 
 	},
 	{
 		"id": "tileitem-06",
